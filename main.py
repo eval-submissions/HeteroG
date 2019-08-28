3230ab55d62d7889c45e50934cc3e74a2bdb04ca
Shiwei Zhang <ylxdzsw@gmail.com>
2019-08-28 19:31:07 +0800

    update

--- main.py
+++ main.py
@@ -11,19 +11,73 @@ optimizer = tf.train.GradientDescentOptimizer(0.2).minimize(tf.reduce_sum(loss))
 dag = optimizer.graph.as_graph_def().node
 
 class AbstractComputationGraph():
-    def __init__(self):
-        self.name_map = {}
-    
-    def add_node(self, node):
-        self.name_map[node.name] = TaggedNode(node)
-    
+    def __init__(self, nodes):
+        self.name_map = { node.name: TaggedNode(node) for node in nodes }
+        for node in self.name_map.values():
+            node.link_inputs(self.name_map)
+
     def tag(self):
         pass
 
+    def replicate_all(self):
+        for name, node in self.name_map:
+            if hasattr(node, 'replicate'):
+                continue
 
+            # try to replicated
+            if node.raw_node.op == 'Const':
+                node.replicate()
+            else:
+                possible_specs = replication_specs[node.raw_node.op].merge_spec
+                for i, (input, index) in enumerate(node.raw_node.input):
+                    if not hasattr(input, 'replication'):
+                        input.replicate()
+
+
+
+            if not hasattr(node, 'replicate'):
+                self.replicate(node)
+
+    def replicate(self, node):
+        pass            
+            
+# naming scheme: for replicas, <origin name>/replica_x where x is the device id
+# for auxiliary nodes, <parent node>/aux_x where x is a local increament number
+
+
+
+# raw_node: the original node
+# inputs/control: linked input and control nodes
+# replicated: if this operation is replicated
+# spec: the output part of spec. This field exists if and only if `replicated` exists
+# outputs: a list of output info (replicated, split/merge method)
 class TaggedNode():
     def __init__(self, node):
         self.raw_node = node
+
+        if node.op == 'Const': # always replicate Const. They won't be actaully used if we merge them immediatly, especially if we prune the graph
+            self.replicated = True
+
+        self._replicability_split = None
+        self._replicability_merge = None
+
+    def link_inputs(self, name_map):
+        self.inputs = []
+        self.control = []
+        for input in self.raw_node.input:
+            if input[0] == '^':
+                self.control.append(input[1:])
+            elif ':' in input:
+                name, index = input.split(':')
+                self.inputs.append((name_map[name], int(index)))
+            else:
+                self.inputs.append((name_map[name], 0))
+
+    def replicability_split(self, inputs):
+        if self._replicability is not None:
+            return self._replicability
+        
+        
 
 
 # main API: replace an op with another op which is backed by a distributed graph
@@ -32,20 +86,13 @@ def distributify(op):
     # prune the graph
     # init = tf.global_variables_initializer()
     # dag = tf.graph_util.extract_sub_graph(dag, [op.name, init.name])
-    acg = AbstractComputationGraph()
-    for node in dag.node:
-        acg.add_node(node)
+    acg = AbstractComputationGraph(dag.node)
     acg.tag()
     dag = acg.assemble()
     graph = tf.Graph()
     with graph.as_default():
         tf.import_graph_def(dag)
     return graph
-
-def duplicate_all(n=2):
-    new_gd = tf.Graph().as_graph_def()
-
-
 
 with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     sess.run(tf.global_variables_initializer())
@@ -70,16 +117,22 @@ class ReplicationSpec():
         self.split_spec = [([tuplify(x) for x in input], (tuplify(x) for x in output)) for input, output in split_spec]
         self.merge_spec = [([tuplify(x) for x in input], (tuplify(x) for x in output)) for input, output in merge_spec]
 
+def get_or_last(list, x):
+    if x < len(list):
+        return list[x]
+    else:
+        return list[-1]
+
 def replication_specs():
     none, copy, cache, sum, fetch, split = ReplicationMethod
     cs = (cache, split)
     cc = (copy, cache)
     pure = ([cc], [copy])
     default = ([cs], [cache])
-    same = [ ([cache, cs], [cache, none]), ([split, cs], [split, none]) ]
-    same2 = [ ([cache, cache, cs], [cache, cache, none]), ([split, split, cs], [split, none]) ] # TODO: require only one split to allow split?
-    batch = ([split, cc], [split])
-    batch2 = ([split, split, cc], [split]) # TODO: require only one split to allow split?
+    same = [ ([cache, cs], [cache, none]), ([cs], [split, none]) ]
+    same2 = [ ([cache, cache, cs], [cache, none]), ([cs], [split, none]) ]
+    batch = [ ([split, cc], [split]) ]
+    batch2 = [ ([cache, split, cc], [split]), ([split, cc], [split]) ]
     grad = ([split, cc], [sum])
     return {
         # special
@@ -97,30 +150,31 @@ def replication_specs():
         # ignore (cannot replicate)
         # ApplyGradientDescent
         # Assign
-        
+
         # element-wise
-        "Identity": ReplicationSpec([ *same ], [ pure, batch ]),
-        "Add": ReplicationSpec([ *same2 ], [ pure, batch2 ]),
-        "Sub": ReplicationSpec([ *same2 ], [ pure, batch2 ]),
-        "Mul": ReplicationSpec([ *same2 ], [ pure, batch2 ]),
-        "Exp": ReplicationSpec([ *same ], [ pure, batch ]),
-        "Neg": ReplicationSpec([ *same ], [ pure, batch ]),
-        "Log1p": ReplicationSpec([ *same ], [ pure, batch ]),
-        "ZerosLike": ReplicationSpec([ *same ], [ pure, batch ]),
-        "GreaterEqual": ReplicationSpec([ *same2 ], [ pure, batch2 ]),
+        "Identity": ReplicationSpec([ *same ], [ pure, *batch, ([sum], [sum]) ]),
+        "Add": ReplicationSpec([ *same2 ], [ pure, *batch2 ]),
+        "Sub": ReplicationSpec([ *same2 ], [ pure, *batch2 ]),
+        "Mul": ReplicationSpec([ *same2 ], [ pure, *batch2 ]),
+        "Exp": ReplicationSpec([ *same ], [ pure, *batch ]),
+        "Neg": ReplicationSpec([ *same ], [ pure, *batch ]),
+        "Log1p": ReplicationSpec([ *same ], [ pure, *batch ]),
+        "ZerosLike": ReplicationSpec([ *same ], [ pure, *batch ]),
+        "GreaterEqual": ReplicationSpec([ *same2 ], [ pure, *batch2 ]),
 
         # element-wise * N
         "AddN": ReplicationSpec([ ([cache], [cache]), ([split], [split, none]) ], [ pure, ([split], [split]) ]),
 
         # nn operations
-        "MatMul": ReplicationSpec([ *same ], [ pure, batch ]),
-        "Reciprocal": ReplicationSpec([ *same ], [ pure, batch ]), # this is not true?
-        "BiasAdd": ReplicationSpec([ *same ], [ pure, batch ]),
+        "MatMul": ReplicationSpec([ *same ], [ pure, *batch ]),
+        "Reciprocal": ReplicationSpec([ *same ], [ pure, *batch ]), # this is not true?
+        "BiasAdd": ReplicationSpec([ *same ], [ pure, *batch ]),
         "BiasAddGrad": ReplicationSpec([ default ], [ pure, grad ]),
-        "SoftMax": ReplicationSpec([ *same ], [ pure, batch ]),
+        "SoftMax": ReplicationSpec([ *same ], [ pure, *batch ]),
     }
 
 replication_specs = replication_specs()
 
 # constraints
 # 1. currently we require all inputs are replicated, this prevents operation that has one input already replicated while other need to be all-reduced
+# 2. most specs (except for identity) currently only pass down batchiness, if one apply operators to gradients (have the merge type "sum"), they won't get replicated
