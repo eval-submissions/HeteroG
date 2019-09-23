aaef56e5664ee8e4d2cfd3215faf16372bae9683
Shiwei Zhang <ylxdzsw@gmail.com>
2019-09-23 11:31:04 +0800

    profiler

--- profiler.py
+++ profiler.py
@@ -1,32 +1,103 @@
 import os
 import time
+import tensorflow as tf
+import tensorflow.core.framework as tfpb
+import operator
+from functools import reduce
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-cache = {}
+cache = {} # TODO: LRU?
 
-def prof_comp(list, index):
-    import tensorflow as tf
+import utils
+op_def_dict = utils.op_def_dict()
+
+def _mkinput(node, dtype, size):
+    if dtype < 100:
+        ts = tf.DType(dtype)
+        if not ts.is_integer and not ts.is_floating: # complex, string, or other unhandleable fancy things
+            raise Exception("not implemented")
+
+        node.op = 'Const'
+        node.attr['dtype'].CopyFrom(tfpb.attr_value_pb2.AttrValue(type=dtype))
+        node.attr['value'].CopyFrom(tfpb.attr_value_pb2.AttrValue(tensor={
+            'dtype': dtype, 'tensor_shape': { 'dim': size }, 'tensor_content': [0] * reduce(operator.mul, size, ts.size)
+        }))
+    else:
+        node.op = 'VariableV2'
+        raise Exception("not implemented")
+
+# node_def is a normal node def but instead of the name, the inputs are replaced by the sizes seperated by commas (and empty name means scalar)
+def _prepare_graph(node_def, graph_def):
+    type_list = []
+    op_def = op_def_dict[node_def.op]
+    for input_arg in op_def.input_arg:
+        if input_arg.type:
+            t = input_arg.type
+        elif input_arg.type_attr:
+            t = node_def.attr[input_arg.type_attr]
+
+        for i in range(node_def.attr.get(input_arg.number_attr, 1)):
+            type_list.append(t)
+
+    input_nodes = []
+    for (i, (t, size)) in enumerate(zip(type_list, node_def.input)):
+        size = [int(x) for x in size.split(',') if x] # split gives an empty element if size is empty
+        if t >= 100: # need Variable, skip them for now
+            print(node_def.op, t)
+            return 0
+
+        node = graph_def.node.add()
+        node.name = 'input_{}'.format(i)
+        node.device = node_def.device
+        _mkinput(node, t, size)
+
+    profilee = graph_def.node.add()
+    profilee.CopyFrom(node_def)
+    profilee.input = ['input_{}'.format(i) for i in range(node_def.input)]
+
+    return gdef
+
+def profile(node_def_raw, target):
+    if node_def_raw in cache:
+        return cache[node_def_raw]
+
+    node_def = tfpb.node_def_pb2.NodeDef()
+    node_def.ParseFromString(node_def_raw)
+
+    tf.reset_default_graph()
+    gdef = tf.get_default_graph().as_graph_def()
+
+    _prepare_graph(gdef)
+    tf.import_graph_def(gdef)
+
+    sess = tf.Session(target)
+    run_meta = tf.compat.v1.RunMetadata()
+    run_opt = tf.compat.v1.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True)
+    sess.run(tf.get_default_graph().get_operation_by_name('import/profilee'),
+        options=run_opt, run_metadata=run_meta)
 
 
-def prof_link(a, b, size):
-    pass
+
 
 # whether this script should be run in an independent process is not decided
-# independent: they are natually decoupled, the result can be for to several runs
+# independent: they are natually decoupled, the result can be used for to several runs
 # not independent: they share the same tf session which is nice
 
-# format: most parameters (type (comp/link) and input sizes) are in the URL
-# the body only contains graphdef (with a special name profilee), which include op and device (and don't forget attr)
-# just ignore variable-related ops? (Assign*, *Apply*, Variable*, https://github.com/tensorflow/tensorflow/blob/r1.14/tensorflow/python/framework/graph_util_impl.py#L35)
 
-class Handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        pass
+# class Handler(BaseHTTPRequestHandler):
+#     def do_POST(self):
+#         buf = self.rfile.read() # the name field should always be called "profilee" so we can match the cache by bytes
 
-try:
-    # parse commandline to get device information
-    HTTPServer(('0.0.0.0', 3907), Handler).serve_forever()
-except KeyboardInterrupt:
-    print("bye~")
+#         node_def = tfpb.node_def_pb2.NodeDef()
+#         node_def.ParseFromString(buf)
+
+#         profile
+
+# try:
+#     # parse commandline to get device information
+#     HTTPServer(('0.0.0.0', 3907), Handler).serve_forever()
+# except KeyboardInterrupt:
+#     print("bye~")
 
 # TODO: concurrent profiling if the devices do not overlap? I don't know if tf sessions are thread-safe
+# TODO: transfering profiling. currently we just accept a bandwidth matrix (can be generagted using funciton in util) along with the topology
--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
-import tensorflow as tf
-import urllib.request
-import time
-
 def get_device_list():
     from tensorflow.python.client import device_lib
     return [x.name for x in device_lib.list_local_devices()]
@@ -12,6 +8,10 @@ def write_tensorboard(graph):
     writer.flush()
 
 def restart_workers(workers):
+    import tensorflow as tf
+    import urllib.request
+    import time
+
     param = '/'.join(server.replace(':', '%3A') for server in workers)
     for task_id, server in enumerate(workers):
         if task_id == 0:
@@ -19,3 +19,12 @@ def restart_workers(workers):
         url = "http://{}:3905/{}/restart/{}/{}".format(server.split(':')[0], int(time.time()) + 10, task_id, param)
         assert urllib.request.urlopen(url).read() == b'ok'
     time.sleep(1)
+
+def op_def_dict():
+    import tensorflow.core.framework as tfpb
+    from google.protobuf import text_format
+
+    oplist = tfpb.op_def_pb2.OpList()
+    text_format.Parse(open("ops.pbtxt").read(), oplist)
+
+    return { op.name: op for op in oplist.op }
