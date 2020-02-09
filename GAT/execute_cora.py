7740576564e5e89059b31140faf1de0b56998da5
Yi Xiaodong <409070169@qq.com>
2020-02-09 21:10:43 +0800

    enlarge models and use two gpu

--- GAT/execute_cora.py
+++ GAT/execute_cora.py
@@ -33,7 +33,7 @@ if os.path.exists("config.txt"):
         config_dict = json.load(f)
 
 # training params
-os.environ["CUDA_VISIBLE_DEVICES"]=config_dict.get("CUDA_VISIBLE_DEVICES","0")
+os.environ["CUDA_VISIBLE_DEVICES"]=config_dict.get("CUDA_VISIBLE_DEVICES","0,1")
 batch_size = 1
 nb_epochs = 100000
 patience = 100
@@ -693,12 +693,12 @@ class new_place_GNN():
             self.time_ratio = tf.placeholder(dtype=tf.float32, shape=(None,),name="time_ratio")
             self.coef_entropy = tf.placeholder(dtype=tf.float32, shape=(),name="coef_entropy")
             self.mems = [tf.placeholder(tf.float32,[2, None, 64]) for _ in range(3)]
-
-        logits = model.inference(self.ftr_in, 1024, self.nb_node, self.is_train,
-                                 self.attn_drop, self.ffd_drop,
-                                 bias_mat=self.bias_in,
-                                 hid_units=hid_units, n_heads=n_heads,
-                                 residual=residual, activation=nonlinearity)
+        with tf.device("gpu0"):
+            logits = model.inference(self.ftr_in, 1024, self.nb_node, self.is_train,
+                                     self.attn_drop, self.ffd_drop,
+                                     bias_mat=self.bias_in,
+                                     hid_units=hid_units, n_heads=n_heads,
+                                     residual=residual, activation=nonlinearity)
 
         if not transformer:
             log_resh = tf.reshape(logits, [-1, 1024])
@@ -719,16 +719,16 @@ class new_place_GNN():
             self.entropy = tf.reduce_sum(tf.log(self.ps_or_reduce + np.power(10.0, -9)) * self.ps_or_reduce, 1)
             self.entropy = -(tf.reduce_mean(self.entropy) + sum / max_replica_num)
         else:
+            with tf.device("gpu1"):
+                logits=model.inference(logits, max_replica_num*(len(devices)+1)+1, self.nb_node, self.is_train,
+                                self.attn_drop, self.ffd_drop,
+                                bias_mat=self.bias_in,
+                                hid_units=place_hid_units, n_heads=place_n_heads,
+                                residual=residual, activation=nonlinearity)
 
-            logits=model.inference(logits, max_replica_num*(len(devices)+1)+1, self.nb_node, self.is_train,
-                            self.attn_drop, self.ffd_drop,
-                            bias_mat=self.bias_in,
-                            hid_units=place_hid_units, n_heads=place_n_heads,
-                            residual=residual, activation=nonlinearity)
-
-            self.device_choices = list()
-            output = tf.reshape(logits, [-1, max_replica_num*(len(devices)+1)+1])
-            output = tf.layers.dense(output, units=max_replica_num*(len(devices)+1)+1, activation=tf.nn.relu)
+                self.device_choices = list()
+                output = tf.reshape(logits, [-1, max_replica_num*(len(devices)+1)+1])
+                output = tf.layers.dense(output, units=max_replica_num*(len(devices)+1)+1, activation=tf.nn.relu)
             '''
             for i in range(6):
                 output = rel_multihead_attn(
