f8c624b0984911a2861267b6d4dbd714b33656fb
Shiwei Zhang <ylxdzsw@gmail.com>
2020-02-11 20:52:12 +0800

    meta

--- test_random_input.py
+++ test_random_input.py
@@ -12,6 +12,8 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 import numpy as np
 import tensorflow as tf
+import google.protobuf.text_format as pbtf
+from tensorflow.python.client import timeline
 
 opt = model_fn()
 init = tf.global_variables_initializer()
@@ -30,7 +32,7 @@ import tge
 
 g = (tge.TGE(gdef, devices)
     .custom({ node.name: [1, 1, 1] for node in gdef.node })
-    .replace_placeholder(64)
+    # .replace_placeholder(64)
     .verbose()
     .compile()
     .get_result()
@@ -40,17 +42,28 @@ tf.reset_default_graph()
 tf.import_graph_def(g)
 graph = tf.get_default_graph()
 
-# x = graph.get_tensor_by_name("import/Placeholder/replica_0:0")
-# y = graph.get_tensor_by_name("import/Placeholder_1/replica_0:0")
+x = graph.get_tensor_by_name("import/Placeholder/replica_0:0")
+y = graph.get_tensor_by_name("import/Placeholder_1/replica_0:0")
 opt = graph.get_operation_by_name("import/GradientDescent/replica_0")
 init = graph.get_operation_by_name("import/init/replica_0")
 
-# data = { x: np.random.uniform(size=(64, 224, 224, 3)), y: np.random.uniform(size=(64, 1000)) }
+data = { x: np.random.uniform(size=(64, 224, 224, 3)), y: np.random.uniform(size=(64, 1000)) }
 
 sess = tf.Session(server.target)
 sess.run(init)
-# sess.run(opt, data)
-sess.run(opt)
+sess.run(opt, data)
+# sess.run(opt)
+
+run_meta = tf.compat.v1.RunMetadata()
+run_opt = tf.compat.v1.RunOptions(trace_level=tf.RunOptions.FULL_TRACE, output_partition_graphs=True)
+sess.run(opt, data, options=run_opt, run_metadata=run_meta)
+
+with open("meta.pb", "w") as fo:
+    fo.write(pbtf.MessageToString(run_meta))
+
+tl = timeline.Timeline(run_meta.step_stats)
+with open("timeline.json", "w") as fo:
+    fo.write(tl.generate_chrome_trace_format())
 
 print("done")
 
