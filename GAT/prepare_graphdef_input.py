9b6f0e6e4cbeedcfc58f63146e441cf820a3c590
xdyi <409070169@qq.com>
2020-03-25 13:49:27 +0800

    add nccl profile

--- GAT/prepare_graphdef_input.py
+++ GAT/prepare_graphdef_input.py
@@ -16,6 +16,8 @@ import google.protobuf.text_format as pbtf
 import pickle as pkl
 sys.path.append('../')
 from profiler import Profiler
+from profiler import NcclProfiler
+from tensorflow.distribute.cluster_resolver import TFConfigClusterResolver
 
 config_dict =dict()
 if os.path.exists("config.txt"):
@@ -46,12 +48,21 @@ def setup_workers(workers, protocol="grpc"):
         assert urllib.request.urlopen(url).read() == b'ok'
     time.sleep(1)
 
-    return tf.distribute.Server(tf.train.ClusterSpec({
-        "worker": workers
-    }), job_name='worker', task_index=0, protocol=protocol)
 
 workers = ["10.28.1.26:3901", "10.28.1.25:3901","10.28.1.24:3901","10.28.1.17:3901","10.28.1.16:3901"]
-server = setup_workers(workers, "grpc")
+os.environ["TF_CONFIG"] = '{ "cluster": { "worker": ["10.28.1.26:3901", "10.28.1.25:3901","10.28.1.24:3901","10.28.1.17:3901","10.28.1.16:3901"]  }, "task": {"type": "worker", "index": 0} }'
+setup_workers(workers, "grpc")
+
+resolver = TFConfigClusterResolver()
+cluster = resolver.cluster_spec()
+dist = tf.distribute.experimental.MultiWorkerMirroredStrategy(
+    tf.distribute.experimental.CollectiveCommunication.NCCL)
+config = dist.update_config_proto(tf.ConfigProto())
+config.ClearField("device_filters")
+config.allow_soft_placement = True  # log_device_placement=True)
+config.gpu_options.allow_growth = True
+server = tf.distribute.Server(cluster, job_name='worker', task_index=0, protocol="grpc", config=config)
+
 
 def model_fn(model_name,batch_size):
     if model_name=="vgg19":
@@ -90,7 +101,7 @@ def model_fn(model_name,batch_size):
         from tensorflow.contrib.slim.nets import resnet_v2
         x = tf.placeholder(tf.float32, shape=(batch_size, 224, 224, 3))
         y = tf.placeholder(tf.float32, shape=(batch_size,1,1, 1000))
-        output, _ = resnet_v2.resnet_v2_152(x, 1000)
+        output, _ = resnet_v2.resnet_v2_50(x, 1000)
         loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=y, logits=output)
         optimizer = tf.train.GradientDescentOptimizer(0.2).minimize(tf.reduce_sum(loss))
         return optimizer
@@ -132,7 +143,10 @@ def generate_edge_file(gdef,folder):
         item_list = ["\n"+item if i!=0 else item for i,item in enumerate(item_list)]
         f.writelines(item_list)
 
-
+def generate_nccl_model():
+    model = NcclProfiler(devices,server.target).profile()
+    with open("data/nccl_model.pkl","wb") as f:
+        pkl.dump(model,f)
 
 
 def generate_feature_file(folder,index):
@@ -237,3 +251,4 @@ for i in range(len(models)):
     tf.reset_default_graph()
     folder = "data/graph"+str(i+1)+"/"
     generate_feature_file(folder,i)
+generate_nccl_model()
