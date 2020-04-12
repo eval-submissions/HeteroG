92c87b37aebbd6caf04e41cef0b233de2f81ce9e
Shiwei Zhang <ylxdzsw@gmail.com>
2020-04-12 15:25:24 +0800

    grouping

--- utils.py
+++ utils.py
@@ -47,3 +47,53 @@ def adapt_batchsize(profile_data, original_batchsize, new_batchsize, nrep_limit)
 
     data_points = { (node, dev): [(original_batchsize // nrep, cost_list[dev]) for ((_name, nrep), cost_list) in profile_data.items() if _name == node ] for node in nodes for dev in range(ndev) }
     return { (node, nrep): [linear_pred(data_points[(node, dev)], new_batchsize // nrep) for dev in range(ndev)] for node in nodes for nrep in range(1, nrep_limit+1) if new_batchsize % nrep == 0 }
+
+def group_around_topk_costs(gdef, groups, profile_data, k):
+    # cores are the largest cost node in each group, which are used to represent the whole group. Centers are topk cores.
+    def cost(x):
+        return profile_data[(x, 1)][0]
+
+    groups = { gdef.node[i].name: id for i, id in enumerate(groups) } # node -> group
+    cores = {} # group -> core node
+    for node, group in groups.items():
+        if group not in cores or cost(cores[group]) < cost(node):
+            cores[group] = node
+    import heapq
+    centers = heapq.nlargest(k, cores.values(), key=cost)
+
+    import networkx as nx
+    G = nx.Graph()
+    for node in gdef.node:
+        name = node.name
+        G.add_node(name)
+        for input in node.input:
+            if input[0] == '^':
+                x = input[1:]
+            else:
+                x = input.split(':')[0]
+            G.add_edge(name, x)
+
+    for center in centers:
+        for node, distance in nx.single_source_shortest_path_length(G, center).items():
+            G.nodes[node][center] = distance
+
+    new_groups = {} # group -> new_group
+    for group, core in cores.items():
+        if core in centers:
+            new_groups[group] = group
+            continue
+        center = min(G.nodes[core], key=G.nodes[core].get, default=None)
+        new_groups[group] = groups.get(center, 0)
+
+    id = 0
+    new_id = {} # old_id -> new_id
+    result = []
+    for node in gdef.node:
+        old_group = groups[node.name]
+        new_group = new_groups[old_group]
+        if new_group not in new_id:
+            new_id[new_group] = id
+            id += 1
+        result.append(new_id[new_group])
+
+    return result
