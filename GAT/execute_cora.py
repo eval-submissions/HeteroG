07a3329413387560c5a7848b94ebd6c3eff4eff5
Yi Xiaodong <409070169@qq.com>
2020-05-18 13:09:37 +0800

    remove last dense layer

--- GAT/execute_cora.py
+++ GAT/execute_cora.py
@@ -551,7 +551,7 @@ class Graph_item():
         self.co_entropy = self.small_co
         self.place_lr = lr
         self.record_time =[]
-        self.mems = [np.zeros([128, bsz, d_model], dtype=np.float32) for layer in range(n_layer)]
+        self.mems = [np.zeros([128, bsz, (max_replica_num+1)*(len(devices))+2], dtype=np.float32) for layer in range(n_layer)]
     def get_colocation_group(self):
         leaders = []
         group = []
@@ -941,9 +941,9 @@ class new_place_GNN():
             initializer = tf.initializers.random_normal(
                 stddev=0.02,
                 seed=None)
-            output,self.new_mems = transformer(log_resh,self.mems,n_layer,d_model,n_head,d_head,d_inner,0.1,0.1,initializer,True,mem_len=128)
-            output = tf.reshape(output, [-1,d_model])
-            output = tf.layers.dense(output,(max_replica_num+1)*(len(devices))+2)
+            output,self.new_mems = transformer(log_resh,self.mems,n_layer,(max_replica_num+1)*(len(devices))+2,n_head,d_head,d_inner,0.1,0.1,initializer,True,mem_len=128)
+            output = tf.reshape(output, [-1,(max_replica_num+1)*(len(devices))+2])
+            #output = tf.layers.dense(output,(max_replica_num+1)*(len(devices))+2)
 
             sum = 0
             for i in range(0,len(devices)):
