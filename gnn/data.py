1420e420d4dc065981b6d3a331e7d19dd467acc6
Shiwei Zhang <ylxdzsw@gmail.com>
2020-09-21 23:53:39 +0800

    data

--- gnn/data.py
+++ gnn/data.py
@@ -5,26 +5,23 @@ import pickle
 import math
 import itertools
 import networkx as nx
-from utils import groupby, car, cadr, cdr
+from utils import groupby, car, cadr, cdr, info
 
-import sys
-def info(*args):
-    print(*args, file=sys.stdout, flush=True)
-
-def gen_topo(devices, inter=2810, intra=2810):
-    g = dgl.DGLGraph()
-    g.add_nodes(len(devices))
-    nfeats = [[time_ratio / 10, memory / 10_000_000_000] for _, time_ratio, memory in devices]
-    efeats = []
+def gen_data(gdef, prof_data, op_table, devices, inter=2810, intra=2810):
+    edge_link = [], []
+    link_feats = []
+    device_feats = [[time_ratio / 10, memory / 10_000_000_000] for _, time_ratio, memory in devices]
     tasks = {}
     for i, (name, *_) in enumerate(devices):
         task = re.search("task:(\d+)/", name)[1]
         if task in tasks:
             for other in tasks[task]:
-                g.add_edge(other, i)
-                g.add_edge(i, other)
-                efeats.append([0, intra / 100_000, math.log(intra) / 10])
-                efeats.append([0, intra / 100_000, math.log(intra) / 10])
+                edge_link[0].append(i)
+                edge_link[1].append(other)
+                edge_link[0].append(other)
+                edge_link[1].append(i)
+                link_feats.append([0, intra / 100_000, math.log(intra) / 10])
+                link_feats.append([0, intra / 100_000, math.log(intra) / 10])
             tasks[task].append(i)
         else:
             tasks[task] = [i]
@@ -33,12 +30,14 @@ def gen_topo(devices, inter=2810, intra=2810):
             for another_task, other_devs in tasks.items():
                 if another_task != task:
                     for another_dev in other_devs:
-                        g.add_edge(dev, another_dev)
-                        g.add_edge(another_dev, dev)
-                        efeats.append([1, inter / 100_000, math.log(inter) / 10])
-                        efeats.append([1, inter / 100_000, math.log(inter) / 10])
-    bandwidth = [x for _, x, _ in efeats]
-    groups = k_spanning_tree(g, bandwidth, 2) + k_spanning_tree(g, bandwidth, 4) + [[0]] + [list(range(len(devices)))]
+                        edge_link[0].append(dev)
+                        edge_link[1].append(another_dev)
+                        edge_link[0].append(another_dev)
+                        edge_link[1].append(dev)
+                        link_feats.append([1, inter / 100_000, math.log(inter) / 10])
+                        link_feats.append([1, inter / 100_000, math.log(inter) / 10])
+    # bandwidth = [x for _, x, _ in link_feats]
+    # tgroups = k_spanning_tree(g, bandwidth, 2) + k_spanning_tree(g, bandwidth, 4) + [[0]] + [list(range(len(devices)))]
 
     base_nccl_model = [0.043420241077615454, 368.2013618677043, 0.27766802543921265, 211.91926070037152]
     nccl_models = {}
@@ -51,18 +50,16 @@ def gen_topo(devices, inter=2810, intra=2810):
         devs = [dgroups[t][0] for t in tasks] # the first (alphabet order) device is the leader of the task
         nccl_models[','.join(sorted(devs))] = [ x * 2810 / inter for x in base_nccl_model ]
 
-    return { "devices": devices, "graph": g, "nfeats": nfeats, "efeats": efeats, "groups": groups, "inter": inter, "intra": intra, "nccl_models": nccl_models }
-
-def gen_data(gdef, prof_data, topo, op_table):
-    g = dgl.DGLGraph()
-    g.add_nodes(len(gdef.node))
-    ntypes = []
+    op_types = []
     for node in gdef.node:
         if node.op not in op_table:
             op_table[node.op] = len(op_table)
-        ntypes.append(op_table[node.op])
-    nfeats = [[np.mean(prof_data[(node.name, nrep)]) / 10_000 for nrep in (1, 2, 4, 8)] for node in gdef.node]
-    efeats = []
+        op_types.append(op_table[node.op])
+    op_feats = [[np.mean(prof_data[(node.name, nrep)]) / 10_000 for nrep in (1, 2, 4, 8)] for node in gdef.node]
+    prev_feats = []
+    succ_feats = []
+    edge_prev = ([], [])
+    edge_succ = ([], [])
     reverse_dict = { node.name: i for i, node in enumerate(gdef.node) }
     for i, node in enumerate(gdef.node):
         for input in node.input:
@@ -75,8 +72,10 @@ def gen_data(gdef, prof_data, topo, op_table):
                     input_index = int(input.split(':')[1])
                 except:
                     input_index = 0
-            g.add_edge(i, reverse_dict[x])
-            g.add_edge(reverse_dict[x], i)
+            edge_prev[0].append(i)
+            edge_prev[1].append(reverse_dict[x])
+            edge_succ[0].append(reverse_dict[x])
+            edge_succ[1].append(i)
             try:
                 shape = [ dim.size for dim in gdef.node[1].attr["_output_shapes"].list.shape[input_index] ]
                 if shape[0] == -1:
@@ -89,41 +88,36 @@ def gen_data(gdef, prof_data, topo, op_table):
                     tensorsize *= size
             except:
                 tensorsize = -1
-            efeats.append([1, tensorsize / 100_000_000])
-            efeats.append([-1, tensorsize / 100_000_000])
-    prof_data = { key: [int(np.mean(times) * time_ratio) for _, time_ratio, _ in topo["devices"]] for key, times in prof_data.items() }
+            prev_feats.append([tensorsize / 100_000_000])
+            succ_feats.append([tensorsize / 100_000_000])
+    prof_data = { key: [int(np.mean(times) * time_ratio) for _, time_ratio, _ in devices] for key, times in prof_data.items() }
 
-    def group_with_layer_name():
-        group_table = {}
-        for i, node in enumerate(gdef.node):
-            if node.name.startswith("GradientDescent") or node.name.startswith("gradients"):
-                prefix = '/'.join(node.name.split('/')[1:3])
-            else:
-                prefix = '/'.join(node.name.split('/')[:2])
-            if prefix in group_table:
-                group_table[prefix].append(i)
-            else:
-                group_table[prefix] = [i]
-        return list(group_table.values())
+    # def group_with_layer_name():
+    #     group_table = {}
+    #     for i, node in enumerate(gdef.node):
+    #         if node.name.startswith("GradientDescent") or node.name.startswith("gradients"):
+    #             prefix = '/'.join(node.name.split('/')[1:3])
+    #         else:
+    #             prefix = '/'.join(node.name.split('/')[:2])
+    #         if prefix in group_table:
+    #             group_table[prefix].append(i)
+    #         else:
+    #             group_table[prefix] = [i]
+    #     return list(group_table.values())
 
-    def group_with_k_spanning_tree():
-        seed = [i for i, node in enumerate(gdef.node) if node.name == 'GradientDescent'][0]
-        return k_spanning_tree(g, [x[1] for x in efeats], 20, seed)
+    # def group_with_k_spanning_tree():
+    #     seed = [i for i, node in enumerate(gdef.node) if node.name == 'GradientDescent'][0]
+    #     return k_spanning_tree(g, [x[1] for x in efeats], 20, seed)
 
-    def group_with_topk_nodes():
-        from utils import group_around_topk_costs
-        from tge import TGE
+    # def group_with_topk_nodes():
+    #     from utils import group_around_topk_costs
+    #     from tge import TGE
 
-        base_groups = TGE(gdef, [dev for dev, _, _ in topo["devices"]]).get_groups()
-        id_list = group_around_topk_costs(gdef, base_groups, prof_data, 19)
-        return list(groupby(enumerate(id_list), key=cadr, value=car).values())
+    #     base_groups = TGE(gdef, [dev for dev, _, _ in devices]).get_groups()
+    #     id_list = group_around_topk_costs(gdef, base_groups, prof_data, 19)
+    #     return list(groupby(enumerate(id_list), key=cadr, value=car).values())
 
-    cgroups = group_with_topk_nodes()
-
-    # for group in cgroups:
-    #     for g in group:
-    #         info(gdef.node[g].name)
-    #     info()
+    # cgroups = group_with_topk_nodes()
 
     # out_of_group = []
     # for i in range(len(gdef.node)):
@@ -134,30 +128,46 @@ def gen_data(gdef, prof_data, topo, op_table):
     #         out_of_group.append(i)
     # info(cgroups, out_of_group)
 
+    edge_place = ([], [])
+    edge_serve = ([], [])
+    for i in range(len(gdef.node)):
+        for j in range(len(devices)):
+            edge_place[0].append(i)
+            edge_place[1].append(j)
+            edge_serve[0].append(j)
+            edge_serve[1].append(i)
+
+    g = dgl.heterograph({
+        ('device', 'link', 'device'): edge_link,
+        ('op', 'prev', 'op'): edge_prev,
+        ('op', 'succ', 'op'): edge_succ,
+        ('op', 'place', 'device'): edge_place,
+        ('device', 'serve', 'op'): edge_serve
+    })
+
     return {
+        "graph": g,
         "gdef": gdef,
         "prof_data": prof_data,
-        "devices": topo["devices"],
-        "cgraph": g,
-        "cgroups": None, # cgroups,
-        "cnfeats": nfeats,
-        "cntypes": ntypes,
-        "cefeats": efeats,
-        "tgraph": topo["graph"],
-        "tgroups": None, # topo["groups"],
-        "tnfeats": topo["nfeats"],
-        "tefeats": topo["efeats"],
-        "topoid": topo["id"],
+        "devices": devices,
+        "op_groups": None, # cgroups,
+        "device_groups": None, # groups,
+        "op_feats": op_feats,
+        "op_types": op_types,
+        "prev_feats": prev_feats,
+        "succ_feats": succ_feats,
+        "device_feats": device_feats,
+        "link_feats": link_feats,
         "op_table": op_table,
         # the two are workarounds; should write a graph parser in tge.py to get the links and bandwidth from graph
-        "inter": topo["inter"],
-        "intra": topo["intra"],
-        "nccl_models": topo["nccl_models"]
+        "inter": inter,
+        "intra": intra,
+        "nccl_models": nccl_models
     }
 
 def get_all_data():
     models = [pickle.load(open("{}.pickle".format(m), "rb")) for m in ("vgg", "resnet", "inception", )] #  , "mlp", "lenet"
-    topos1 = [gen_topo([
+    topo_spec1 = [([
         ("/job:worker/replica:0/task:0/device:GPU:0", 1, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:1", 1, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:2", 1, 3<<30),
@@ -166,28 +176,25 @@ def get_all_data():
         ("/job:worker/replica:0/task:0/device:GPU:5", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:6", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:7", 1.5, 3<<30),
-    ], intra=bandwidth) for bandwidth in (1000, 10000)]
-    topos2 = [gen_topo([
+    ], 2810, bandwidth) for bandwidth in (1000, 10000)]
+    topo_spec2 = [([
         ("/job:worker/replica:0/task:0/device:GPU:0", 1, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:1", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:2", 2, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:3", 2.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:4", 3, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:5", 3.5, 3<<30),
-    ], intra=bandwidth) for bandwidth in (400, 4000, 40000)]
-    topos3 = [gen_topo([
+    ], 2810, bandwidth) for bandwidth in (400, 4000, 40000)]
+    topo_spec3 = [([
         ("/job:worker/replica:0/task:0/device:GPU:0", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:1", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:2", 1.5, 3<<30),
         ("/job:worker/replica:0/task:0/device:GPU:3", 1.5, 3<<30),
         ("/job:worker/replica:0/task:1/device:GPU:0", 1, 3<<30),
         ("/job:worker/replica:0/task:1/device:GPU:1", 1, 3<<30),
-    ], intra=bandwidth, inter=100) for bandwidth in (4000, 40000)]
-    topos = topos1 + topos2 + topos3
-    for i in range(len(topos)):
-        topos[i]["id"] = i
+    ], 100, bandwidth) for bandwidth in (4000, 40000)]
     op_table = {}
-    return [gen_data(gdef, prof_data, topo, op_table) for gdef, prof_data in models for topo in topos]
+    return [gen_data(gdef, prof_data, op_table, devices, inter, intra) for gdef, prof_data in models for devices, inter, intra in topo_spec1 + topo_spec2 + topo_spec3]
 
 # prim's algorithm
 # alternative: https://networkx.github.io/documentation/stable/reference/algorithms/tree.html#module-networkx.algorithms.tree.mst
